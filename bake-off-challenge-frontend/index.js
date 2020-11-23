a2f85ad4012aa14d2af21ee9e4b8f476f49608d6
jomariepolanco <jomariepolanco@gmail.com>
2020-11-23 15:17:14 -0500

    2nd deliverable done

--- bake-off-challenge-frontend/index.js
+++ bake-off-challenge-frontend/index.js
@@ -1,2 +1,42 @@
 // your code here!
-console.log("🥧")
+// console.log("🥧")
+const bakeSideBar = document.querySelector("#bakes-container")
+const mainView = document.querySelector("#detail")
+
+bakeSideBar.addEventListener("click", event => {
+    const id = event.target.dataset.id 
+    getOneBake(id)
+})
+
+const renderMainView = bake => {
+    const img = mainView.querySelector("img")
+    const h1 = mainView.querySelector("h1")
+    const p = mainView.querySelector(".description")
+    const rateForm = mainView.querySelector("#score-form")
+    img.src = bake.image_url
+    img.alt = bake.name 
+    h1.textContent = bake.name 
+    p.textContent = bake.description
+    rateForm.dataset.id = bake.id 
+    rateForm.score.value = bake.score
+}
+const renderBakeSideBar = bake => {
+    const li = document.createElement("li")
+    li.textContent = bake.name
+    li.dataset.id = bake.id 
+    bakeSideBar.append(li)
+}
+
+const getOneBake = id => {
+    return fetch(`http://localhost:3000/bakes/${id}`)
+        .then(resp => resp.json())
+        .then(oneBake => renderMainView(oneBake))
+}
+
+const getBakes = () => {
+    fetch('http://localhost:3000/bakes')
+        .then(resp => resp.json())
+        .then(allBakes => allBakes.forEach(bake => renderBakeSideBar(bake)))
+}
+getBakes()
+getOneBake(1)
